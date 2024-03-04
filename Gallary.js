403afde29ff52d5940abce1942fcacfd839b4e27
Faith nkatha <faithnkatha160@gmail.com>
2024-03-04 23:57:39 +0300

    Add files via upload

---
+++ Gallary.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Profile from "./Profile"
+
+function Gallary(){
+    return(
+        <section>
+            <Profile />
+            <Profile />
+            <Profile />
+        </section>
+    );
+
+}
+export default Gallary
---
+++ Greet.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Greet(props){
+ return<h1>Hello {props.name} ama {props.Heroname} </h1>   
+}
+
+export default Greet
